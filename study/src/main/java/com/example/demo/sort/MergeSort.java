1f797dbe226e1c46b455d85ddd06939d4ef0a479
xianyang.fang@onesmart.org <Fxy@1994>
2020-12-10 16:56:10 +0800

    快速排序算法

--- study/src/main/java/com/example/demo/sort/MergeSort.java
+++ study/src/main/java/com/example/demo/sort/MergeSort.java
@@ -4,43 +4,99 @@ public class MergeSort {
 
     public static void main(String[] args) {
         int[] arr = {11,44,23,67,88,65,34,48,9,12};
-        int[] tmp = new int[arr.length];    //新建一个临时数组存放
-        mergeSort(arr,0,arr.length-1,tmp);
+        mergeSort(arr,arr.length);
         for(int i=0;i<arr.length;i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    public static void merge(int[] arr,int low,int mid,int high,int[] tmp){
-        int i = 0;
-        int j = low,k = mid+1;  //左边序列和右边序列起始索引
-        while(j <= mid && k <= high){
-            if(arr[j] < arr[k]){
-                tmp[i++] = arr[j++];
-            }else{
-                tmp[i++] = arr[k++];
+
+    // 归并排序算法, a是数组，n表示数组大小
+    public static void mergeSort(int[] a, int n) {
+        mergeSortInternally(a, 0, n-1);
+    }
+
+    // 递归调用函数
+    private static void mergeSortInternally(int[] a, int p, int r) {
+        // 递归终止条件
+        if (p >= r) return;
+
+        // 取p到r之间的中间位置q,防止（p+r）的和超过int类型最大值
+        int q = p + (r - p)/2;
+        // 分治递归
+        mergeSortInternally(a, p, q);
+        mergeSortInternally(a, q+1, r);
+
+        // 将A[p...q]和A[q+1...r]合并为A[p...r]
+        merge(a, p, q, r);
+    }
+
+    private static void merge(int[] a, int p, int q, int r) {
+        int i = p;
+        int j = q+1;
+        int k = 0; // 初始化变量i, j, k
+        int[] tmp = new int[r-p+1]; // 申请一个大小跟a[p...r]一样的临时数组
+        while (i<=q && j<=r) {
+            if (a[i] <= a[j]) {
+                tmp[k++] = a[i++]; // i++等于i:=i+1
+            } else {
+                tmp[k++] = a[j++];
             }
         }
-        //若左边序列还有剩余，则将其全部拷贝进tmp[]中
-        while(j <= mid){
-            tmp[i++] = arr[j++];
+
+        // 判断哪个子数组中有剩余的数据
+        int start = i;
+        int end = q;
+        if (j <= r) {
+            start = j;
+            end = r;
         }
 
-        while(k <= high){
-            tmp[i++] = arr[k++];
+        // 将剩余的数据拷贝到临时数组tmp
+        while (start <= end) {
+            tmp[k++] = a[start++];
         }
-        for(int t=0;t<i;t++){
-            arr[low+t] = tmp[t];
+
+        // 将tmp中的数组拷贝回a[p...r]
+        for (i = 0; i <= r-p; ++i) {
+            a[p+i] = tmp[i];
         }
     }
 
-    public static void mergeSort(int[] arr,int low,int high,int[] tmp){
-        if(low<high){
-            int mid = (low+high)/2;
-            mergeSort(arr,low,mid,tmp); //对左边序列进行归并排序
-            mergeSort(arr,mid+1,high,tmp);  //对右边序列进行归并排序
-            merge(arr,low,mid,high,tmp);    //合并两个有序序列
+    /**
+     * 合并(哨兵)
+     *
+     * @param arr
+     * @param p
+     * @param q
+     * @param r
+     */
+    private static void mergeBySentry(int[] arr, int p, int q, int r) {
+        int[] leftArr = new int[q - p + 2];
+        int[] rightArr = new int[r - q + 1];
+
+        for (int i = 0; i <= q - p; i++) {
+            leftArr[i] = arr[p + i];
+        }
+        // 第一个数组添加哨兵（最大值）
+        leftArr[q - p + 1] = Integer.MAX_VALUE;
+
+        for (int i = 0; i < r - q; i++) {
+            rightArr[i] = arr[q + 1 + i];
+        }
+        // 第二个数组添加哨兵（最大值）
+        rightArr[r-q] = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+        int k = p;
+        while (k <= r) {
+            // 当左边数组到达哨兵值时，i不再增加，直到右边数组读取完剩余值，同理右边数组也一样
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
         }
     }
-
 }
